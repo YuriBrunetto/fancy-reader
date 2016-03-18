accb52f92ed869f4e3c2f4eabb0ed1e77f6063e7
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-03-17 23:17:36 -0300

    initial commit

---
+++ reader.js
@@ -0,0 +1,14 @@
+var f_serif = "'Times New Roman', Georgia, serif";
+var f_sans_serif = "'Helvetica Neue', Helvetica, Arial, sans-serif";
+var titles = document.getElementsByTagName("h1, h2");
+var paragraphs = document.getElementsByTagName("p");
+
+console.log(titles.length, paragraphs.length);
+
+for (numTitles = 0; numTitles < titles.length; numTitles++) {
+    titles[numTitles].style.fontFamily = f_serif;
+}
+
+for (numParagrahps = 0; numParagrahps < titles.length; numParagrahps++) {
+    paragraphs[numParagrahps].style.fontFamily = f_sans_serif;
+}
